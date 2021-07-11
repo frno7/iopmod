a08edf41406b841b4f86bb75a755e6899f948f94
Fredrik Noring <noring@nocrew.org>
2021-07-11 18:13:51 +0200

    Define BIT

---
+++ include/iopmod/bits.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef IOPMOD_BITS_H
+#define IOPMOD_BITS_H
+
+#include "iopmod/types.h"
+
+#define BIT(nr)		(((uint32_t)1) << (nr))
+
+#endif /* IOPMOD_BITS_H */
